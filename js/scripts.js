ca7366024a979a496c9f5a00cd7403d4796ae1ad
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-06 07:54:13 +0100

    Add button

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-var btn = document.getElementById('addElem');
-btn.addEventListener('click', function(e) {
+var btn1 = document.getElementById('addElements');
+btn1.addEventListener('click', function(e) {
   console.log('it was clicked');
 
-var listWeb = document.getElementById('list');
-var newElem = document.createElement('li');
-var num = document.getElementsByTagName('li').length;
+var listWeb1 = document.getElementById('endlessList');
+var newElem1 = document.createElement('li');
+var num1 = document.getElementsByTagName('li').length;
 //console.log(num);
 
-newElem.innerHTML = 'item ' + num;
-listWeb.appendChild(newElem);
+newElem1.innerHTML = 'item ' + num1;
+listWeb1.appendChild(newElem1);
+
+});
+
+
+var btn2 = document.getElementById('addOnce');
+btn2.addEventListener('click', function(e) {
+  console.log('it was clicked too');
+
+var listWeb2 = document.getElementById('onceList');
+var newElem2 = document.createElement('li');
+var num2 = document.getElementsByTagName('li').length;
+//console.log(num);
+
+newElem2.innerHTML = 'item ' + num2;
+listWeb2.appendChild(newElem2);
+
+document.getElementById('addOnce').disabled = 'true';
+
 
 });
