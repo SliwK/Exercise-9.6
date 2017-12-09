71f1bd760744c367fbdb82644106b36ae2858042
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-10 00:29:21 +0100

    Correct html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var btn1 = document.getElementById('addElements');
-btn1.addEventListener('click', function(e) {
+btn1.addEventListener('click', function fnk() {
   console.log('it was clicked');
 
 var listWeb1 = document.getElementById('endlessList');
@@ -12,11 +12,11 @@ listWeb1.appendChild(newElem1);
 
 });
 
-
+function addEvent() {
 var btn2 = document.getElementById('addOnce');
-btn2.addEventListener('click', function(e) {
+btn2.addEventListener('click', function fnk1() {
   console.log('it was clicked too');
-
+  var klik = 1;
 var listWeb2 = document.getElementById('onceList');
 var newElem2 = document.createElement('li');
 var num2 = document.getElementsByTagName('li').length;
@@ -25,7 +25,14 @@ var num2 = document.getElementsByTagName('li').length;
 newElem2.innerHTML = 'item ' + num2;
 listWeb2.appendChild(newElem2);
 
-document.getElementById('addOnce').disabled = 'true';
-
+//document.getElementById('addOnce').disabled = 'true';
+if(klik==1){
+  btn2.removeEventListener('click', fnk1);
+}
 
 });
+
+}
+
+
+addEvent();
