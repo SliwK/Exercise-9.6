e11dcd5d204d82c70edd430c6d93d12e0593773b
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-14 21:14:58 +0100

    Modify js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,41 @@
-var btn1 = document.getElementById('addElements');
-btn1.addEventListener('click', function fnk() {
-  console.log('it was clicked');
+//commmon function
+function commonFnk(listWeb) {
+  var newElem = document.createElement('li');
+  var num = document.getElementsByTagName('li').length;
+  //console.log(num);
 
-var listWeb1 = document.getElementById('endlessList');
-var newElem1 = document.createElement('li');
-var num1 = document.getElementsByTagName('li').length;
-//console.log(num);
-
-newElem1.innerHTML = 'item ' + num1;
-listWeb1.appendChild(newElem1);
-
-});
-
-function addEvent() {
-var btn2 = document.getElementById('addOnce');
-btn2.addEventListener('click', function fnk1() {
-  console.log('it was clicked too');
-  var klik = 1;
-var listWeb2 = document.getElementById('onceList');
-var newElem2 = document.createElement('li');
-var num2 = document.getElementsByTagName('li').length;
-//console.log(num);
-
-newElem2.innerHTML = 'item ' + num2;
-listWeb2.appendChild(newElem2);
-
-//document.getElementById('addOnce').disabled = 'true';
-if(klik==1){
-  btn2.removeEventListener('click', fnk1);
+  newElem.innerHTML = 'item ' + num;
+  listWeb.appendChild(newElem);
 }
 
-});
+//endless list creation
+function addEvents() {
+  var btn = document.getElementById('addElements');
+  btn.addEventListener('click', function fnk() {
+  console.log('it was clicked');
 
+  var listWeb = document.getElementById('endlessList');
+
+  commonFnk(listWeb);
+
+});
+}
+
+addEvents();
+
+//one element list creation
+function addEvent() {
+  var btn = document.getElementById('addOnce');
+  btn.addEventListener('click', function fnk() {
+  console.log('it was clicked too');
+
+  var listWeb = document.getElementById('onceList');
+
+  commonFnk(listWeb);
+
+  btn.removeEventListener('click', fnk);
+
+});
 }
 
 
